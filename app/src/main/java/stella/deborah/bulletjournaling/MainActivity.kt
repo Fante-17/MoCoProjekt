9ccd569ce089d17ffde3903fecf9af6053f3a3c7
Ornela  Fante <64073462+Fante-17@users.noreply.github.com>
2022-01-19 22:55:53 +0100

    Add files via upload

---
+++ app/src/main/java/stella/deborah/bulletjournaling/MainActivity.kt
@@ -0,0 +1,11 @@
+package stella.deborah.bulletjournaling
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
