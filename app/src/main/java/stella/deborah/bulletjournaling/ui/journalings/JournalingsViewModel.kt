94e8d9ef0cb8e1797e608157a4aa1e28c5738d6f
Ornela  Fante <64073462+Fante-17@users.noreply.github.com>
2022-02-28 20:06:38 +0100

    Final Results

--- app/src/main/java/stella/deborah/bulletjournaling/ui/journalings/JournalingsViewModel.kt
+++ app/src/main/java/stella/deborah/bulletjournaling/ui/journalings/JournalingsViewModel.kt
@@ -6,7 +6,7 @@ import androidx.lifecycle.ViewModel
 
 class JournalingsViewModel : ViewModel() {
     private val _text = MutableLiveData<String>().apply {
-        value = "This is Journaling Fragment"
+        value = " "
     }
     val text: LiveData<String> = _text
 
